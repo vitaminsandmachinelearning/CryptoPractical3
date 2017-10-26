6e794cfcbd006f2125e64e985f7d4bb66358a723
vitaminsandmachinelearning <32342182+vitaminsandmachinelearning@users.noreply.github.com>
2017-10-26 23:24:14 +0100

    working but slow af

--- src/cryptopractical3/CryptoPractical3.java
+++ src/cryptopractical3/CryptoPractical3.java
@@ -2,12 +2,22 @@
 package cryptopractical3;
 
 import java.security.*;
+import java.util.ArrayList;
 
 public class CryptoPractical3 {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    static char[] charset = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+        'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    //static char[] charset = {'a', 'b', 'c'};
+    static int charmax = 6;
+
+    
+    public static void main(String[] args) throws NoSuchAlgorithmException 
+    {
         String password = ""; 
         System.out.println("Password: " + password + "\nSHA1: " + sha1(password)); 
+        bruteforce("");
     }
     static String sha1(String password) throws NoSuchAlgorithmException
     {
@@ -17,5 +27,28 @@ public class CryptoPractical3 {
         for(int i = 0; i < bytes.length; i++)
             toReturn += Integer.toString((bytes[i]&0xff) + 0x100, 16).substring(1);
         return toReturn;
+    }
+    static String bruteforce(String hash) throws NoSuchAlgorithmException
+    {
+        ArrayList<String> passwords = new ArrayList<String>();
+        passwords.add("");
+        getallstrings(charset, charset.length, 6, "", passwords);
+        for(String s : passwords)
+            System.out.println("0:" + s);
+        return "";
+    }
+    static void getallstrings(char[] set, int n, int length, String p, ArrayList<String> passwords) throws NoSuchAlgorithmException
+    {
+        if(length == 0)
+        {
+            passwords.add(sha1(p));
+            return;
+        }
+        for(int i = 0; i < n; i++)
+        {
+            String np = p + set[i];
+            getallstrings(set, n, length - 1, np, passwords);
+        }
+        
     }
 }
