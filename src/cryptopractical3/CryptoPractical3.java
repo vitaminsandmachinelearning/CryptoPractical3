629639c93516c826b07d5878617e06365e691fc7
vitaminsandmachinelearning <32342182+vitaminsandmachinelearning@users.noreply.github.com>
2017-10-26 19:52:25 +0100

    sha implemented

--- src/cryptopractical3/CryptoPractical3.java
+++ src/cryptopractical3/CryptoPractical3.java
@@ -1,10 +1,21 @@
 
 package cryptopractical3;
 
+import java.security.*;
+
 public class CryptoPractical3 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String password = ""; 
+        System.out.println("Password: " + password + "\nSHA1: " + sha1(password)); 
     }
-    
+    static String sha1(String password) throws NoSuchAlgorithmException
+    {
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        byte[] bytes = md.digest(password.getBytes());
+        String toReturn = "";
+        for(int i = 0; i < bytes.length; i++)
+            toReturn += Integer.toString((bytes[i]&0xff) + 0x100, 16).substring(1);
+        return toReturn;
+    }
 }
